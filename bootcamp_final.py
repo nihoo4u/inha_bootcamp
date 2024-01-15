2ce1497361a4511fac3f5aea4b0259fe6d5c1d7d
pjy <nihoo4u@gmail.com>
2024-01-15 15:55:02 +0900

    day02

--- bootcamp_final.py
+++ bootcamp_final.py
@@ -1,7 +1,11 @@
-letter=input('input the spelling')
-# vowels={'a','e','i','o','u'}
-vowels='aeiou'
-if letter in vowels:
-    print(f'{letter}is  included')
-else:
-    print(f'{letter}is not included')
+while True:
+    menu=int(input("Input the number"))
+    if menu==1:
+        f=float(input('input f'))
+        print(f'f:{f} and c:{(f-32)*5/9:.3f}')
+    elif menu==2:
+        c=float(input('input c'))
+        print(f'c:{c} and f:{c*9/5+32}')
+    else:
+        print('terminate program')
+        break
