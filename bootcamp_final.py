c1a69b0d57a83e314f64a5bbfa9ede5313037d2c
pjy <nihoo4u@gmail.com>
2024-01-15 14:58:52 +0900

    if_1

--- bootcamp_final.py
+++ bootcamp_final.py
@@ -1,4 +1,7 @@
-f=float(input("input temperature"))
-c=(f-32)*5/9
-print(f'패러내이트:{f},셀씨우스:{c:.2f}')
-
+menu=int(input("1)f->c 2>c->f 3)quit"))
+if menu==1:
+    f=float(input('input f'))
+    print(f'f:{f} and c:{(f-32)*5/9:.3f}')
+elif menu==2:
+    c=float(input('input c'))
+    print(f'c:{c} and f:{c*9/5+32}')
