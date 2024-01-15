063bdbcd5be923987d45c84051aa31fd17e1ec68
pjy <nihoo4u@gmail.com>
2024-01-15 15:47:50 +0900

    using_in

--- bootcamp_final.py
+++ bootcamp_final.py
@@ -1,7 +1,7 @@
-menu=int(input("1)f->c 2>c->f 3)quit"))
-if menu==1:
-    f=float(input('input f'))
-    print(f'f:{f} and c:{(f-32)*5/9:.3f}')
-elif menu==2:
-    c=float(input('input c'))
-    print(f'c:{c} and f:{c*9/5+32}')
+letter=input('input the spelling')
+# vowels={'a','e','i','o','u'}
+vowels='aeiou'
+if letter in vowels:
+    print(f'{letter}is  included')
+else:
+    print(f'{letter}is not included')
