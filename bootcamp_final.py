fbd75b5635eb2df8b57e27360a20a6aabfb484b2
pjy <nihoo4u@gmail.com>
2024-01-15 13:55:57 +0900

    base calcultion

--- bootcamp_final.py
+++ bootcamp_final.py
@@ -1,7 +1,6 @@
-num1=int(input("input the number"))
-num2=int(input("input the number"))
-# q=num1//num2
-# r=num1%num2
+dec=65
+octal=0o101
+hexadecimal=0x41
+binary=0b01000001
+print(dec,octal,hexadecimal,binary)
 
-# print(f'몫은 {q}이고 나머지는 {r}입니다')
-print(f'몫은 {divmod(num1,num2)[0]}이고 나머지는 {divmod(num1,num2)[1]}입니다')
