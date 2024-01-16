59ce7fb7d1cd7722ccc06c0b9e135dea1147db10
pjy <nihoo4u@gmail.com>
2024-01-16 14:28:33 +0900

    delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-a=[1,2,3]
-b=a
-a[0]=100
-print(b)
-
-
-base_number=int(input('INput base number: '))
-exponent_number=int(input('INput exponent number: '))
-print(f'밑은 {base_number},"지수는 {exponent_number},결과값은 {base_number**exponent_number}')
