6d0ec5c46ff7313734c6467a14ca7a63cf7ca2dd
pjy <nihoo4u@gmail.com>
2024-01-16 15:24:11 +0900

    v4 primefinder

--- bootcamp_final.py
+++ bootcamp_final.py
@@ -1,19 +1,16 @@
-while True:
-    a=input("input the number")
-    if a=='q':
-        print('terminate the calculator')
-        break
-    if int(a)<=1:
-        print(f'{int(a)} is not a prime number ')
-        continue
+def find_prime(x):
     is_prime=True
-    for i in range(2,int(a)):
-
-        if int(a)%i==0:
+    for i in range(2,x):
+        if x%i==0:
             is_prime=False
-
             break
-    if is_prime:
-        print(f'{int(a)} is a prime number')
-    else:
-        print(f'{int(a)}is not a prime number')
+    return is_prime
+
+
+a,b=map(int,input().split())
+if a>b:
+    (a,b)=(b,a)
+for i in range(a,b+1):
+    if find_prime(i):
+        print(i,end=',')
+
