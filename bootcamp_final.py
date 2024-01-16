9278d08ffd3e1220355b9c30a85d1b1b15e4e096
pjy <nihoo4u@gmail.com>
2024-01-16 14:19:05 +0900

    find_primenumber

--- bootcamp_final.py
+++ bootcamp_final.py
@@ -1,25 +1,21 @@
-# a=[1,2,3,4,5]
-# a=list(map(str,a))
-# a="/".join(a)
-# print(a)
-#
-# a,b=map(int,input("2개의 값을 입력하시오").split())
-# print(a+b)
-#
-# numbers=input().split()
-# print(int(numbers[0])+int(numbers[1]))
-#
-# a='111111111'
-# b=a.replace('1','2',3)
-# print(b)
-#
-# a='..ggogogog.oog....!J'
-# b=a.strip('.!J')
-# print(b)
+while True:
+    a=input()
+    if a=='q':
+        print('terminate the calculator')
+        break
+    s=0
+    for i in range(2,int(a)):
 
-go=input()
-a='he is running on the block'
-if a.find(go)!=-1:
-    print(2)
-else:
-    print(f'we didnt find {go} ')
+        if int(a)%i==0:
+            print(f'{int(a)} is not a prime number ')
+            break
+        s+=1
+    if s==int(a)-2:
+        print(f'{int(a)} is a prime number')
+
+
+
+
+
+
+
