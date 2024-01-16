b4d85e8bd00cb44efdd1c56d9e4fdedba8a79bbd
pjy <nihoo4u@gmail.com>
2024-01-16 13:03:57 +0900

    day03

--- bootcamp_final.py
+++ bootcamp_final.py
@@ -1,11 +1,25 @@
-while True:
-    menu=int(input("Input the number"))
-    if menu==1:
-        f=float(input('input f'))
-        print(f'f:{f} and c:{(f-32)*5/9:.3f}')
-    elif menu==2:
-        c=float(input('input c'))
-        print(f'c:{c} and f:{c*9/5+32}')
-    else:
-        print('terminate program')
-        break
+# a=[1,2,3,4,5]
+# a=list(map(str,a))
+# a="/".join(a)
+# print(a)
+#
+# a,b=map(int,input("2개의 값을 입력하시오").split())
+# print(a+b)
+#
+# numbers=input().split()
+# print(int(numbers[0])+int(numbers[1]))
+#
+# a='111111111'
+# b=a.replace('1','2',3)
+# print(b)
+#
+# a='..ggogogog.oog....!J'
+# b=a.strip('.!J')
+# print(b)
+
+go=input()
+a='he is running on the block'
+if a.find(go)!=-1:
+    print(2)
+else:
+    print(f'we didnt find {go} ')
