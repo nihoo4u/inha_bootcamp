ef712bc6422f8ad5d33da5e3a7389fd2c73621db
pjy <nihoo4u@gmail.com>
2024-01-16 14:26:31 +0900

    change variable s to threshold

--- bootcamp_final.py
+++ bootcamp_final.py
@@ -3,14 +3,14 @@ while True:
     if a=='q':
         print('terminate the calculator')
         break
-    s=0
+    threshold=0
     for i in range(2,int(a)):
 
         if int(a)%i==0:
             print(f'{int(a)} is not a prime number ')
             break
-        s+=1
-    if s==int(a)-2:
+        threshold+=1
+    if threshold==int(a)-2:
         print(f'{int(a)} is a prime number')
 
 
