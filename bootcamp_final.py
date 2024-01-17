c0f708a65716d91f06db30850ac0ba43daae87d2
pjy <nihoo4u@gmail.com>
2024-01-18 00:16:38 +0900

    shallow/deep copy

--- bootcamp_final.py
+++ bootcamp_final.py
@@ -1,23 +1,25 @@
-##dict
-# a=dict()
-cell1={'a':'b','g':'d'}
-# cell3={'d':'b','e':'d','c':'dfdf'}
-cell2=dict(e='f',g='t')
-# li1=[['a','b'],['c','d']]
-li2=['ad','df','fd','fg']
-# cell2['e']='changed'
-# print(cell1.get('g','go home'))
+# import copy
+# subject=[1,2,3]
+# a=subject
+# b=subject[:]
+# c=subject.copy()
+# d=copy.deepcopy(subject)
+# print(subject,a,b,c,d)
+# print(id(subject),id(a),id(b),id(c),id(d))
+# subject[0]=100
+# print(subject,a,b,c,d)
+# print(id(subject),id(a),id(b),id(c),id(d))
 
-# for x,y in cell1.items():
 
-# import random
-# print(random.choice(li2))
+import copy
+subject=2
+a=subject
 
-# print(dict(cell2,**cell1))
-# print({**cell2,**cell1})
+d=copy.deepcopy(subject)
+print(subject,a,d)
+print(id(subject),id(a),id(d))
+subject=3
+print(subject,a,d)
+print(id(subject),id(a),id(d))
 
-# print(cell1.update(cell2))
-# print(cell1)
-# print(cell1.pop('a'))
-del cell1['a']
-print(cell1)
+
