896358a9791bb6d6387b179eb66ce66b3be30616
pjy <nihoo4u@gmail.com>
2024-01-17 13:25:43 +0900

    copy

--- bootcamp_final.py
+++ bootcamp_final.py
@@ -1,21 +1,11 @@
-a,b=input().split()
-a,b=[1,2]
-print(a)
+import copy
 
-a=1,
-print(type(a))
-print(type(1,))
-
-a=(1,2,3)
-b=(2,1)
-print(a<b)
-
-a=[1,2,3,4]
-b=[7,8,9]
-a[1:3]='cat'
-print(a)
-
-a=['dd','aa','vd','123','고고','23']
-b=sorted(a)
-print(a)
-print(b)
+subjects=["a","b","c"]
+a=subjects
+b=subjects.copy()
+c=list(subjects)
+d=subjects[:]
+e=copy.deepcopy(a)
+print(subjects,a,b,c,d)
+subjects[1]="x"
+print(subjects,a,b,c,d)
