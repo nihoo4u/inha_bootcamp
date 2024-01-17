89a9d64c0612d469abe94d67b8e75a3ad4d70937
pjy <nihoo4u@gmail.com>
2024-01-17 16:27:10 +0900

    dict_program

--- bootcamp_final.py
+++ bootcamp_final.py
@@ -1,30 +1,37 @@
-# a=[['a','b'],['c','d']] # list->dict
-# print(dict(a))
-#
-# b=['af','sd','df'] # str->dict
-# print(dict(b))
+import random
+drink_food={'위스키':'초콜릿','와인':'치즈','소주':'삼겹살','고량주':'왕꼬치'}
 
-# c={'a':"c",'b':'d'} #change,add
-# c['a']='sdfsdf'
-# c['c']='dfdfd'
-# print(c)
-#
-# print(c.get('a'))
+del drink_food['위스키']
+
+print(drink_food)
+drink_food_keys=list(drink_food)
+print(drink_food.pop('와인'))
+print(drink_food)
+
+while True:
+    menu=input(f'다음 술 중에 고르시오\n 1){drink_food_keys[0]},2){drink_food_keys[1]},3){drink_food_keys[2]},4){drink_food_keys[3]}')
+    random_drink=random.choice(drink_food_keys)
+    if menu=='1':
+        print(f'{drink_food_keys[0]}에 어울리는 안주는 {drink_food[drink_food_keys[0]]}입니다')
+    elif menu=='2':
+        print(f'{drink_food_keys[1]}에 어울리는 안주는 {drink_food[drink_food_keys[1]]}입니다')
+    elif menu=='3':
+        print(f'{drink_food_keys[2]}에 어울리는 안주는 {drink_food[drink_food_keys[2]]}입니다')
+    elif menu=='4':
+        print(f'{drink_food_keys[3]}에 어울리는 안주는 {drink_food[drink_food_keys[3]]}입니다')
+    else:
+        print(f'{random_drink}에 어울리는 안주는 없습니다')
 
 
-sugang=dict(subject='pytoon',db='kang',cpp='song')
-# for subject,professor in sugang.items():
-#     print(subject,professor)
 
-for k in sugang:
-    print(k, end=' ')
-print()
-for k in sugang.values():
-    print(k, end=' ')
-print()
 
-for k in sugang.keys():
-    print(k, end=' ')
-print()
-for k in sugang.items():
-    print(k)
+# print(drink_food_keys)
+# print(drink_food_keys[0])
+
+# cells={'a':'abc','b':'bcd','c':'cde','d':'def'}
+# print(list(cells.keys()))
+
+# first={'a':'agony','b':'bliss'}
+
+
+
