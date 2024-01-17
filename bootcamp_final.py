3c589314358c3900fd2621f0b9508905b55edec3
pjy <nihoo4u@gmail.com>
2024-01-17 12:53:58 +0900

    day04_1

--- bootcamp_final.py
+++ bootcamp_final.py
@@ -1,76 +1,21 @@
-#4:구간소,3:소수 판정프로그램수1번 화씨 to 섭
-while True:
-    a=int(input())
-    if a==5:
-        print('Terminate the program')
-        break
-    if a==1:
-        f = float(input('input f'))
-        print(f'f:{f} and c:{(f - 32) * 5 / 9:.3f}')
-    elif a==2:
-        c = float(input('input c'))
-        print(f'c:{c} and f:{c * 9 / 5 + 32}')
-    elif a==3:
-        x=int(input())
-        is_prime=True
-        for i in range(2,x):
-            if x%i==0:
-                is_prime=False
-                break
-        if is_prime:
-            print(f'{x} is a prime number')
-        else:
-            print(f'{x} is not a prime number')
-    else:
-        a,b=map(int,input().split())
+a,b=input().split()
+a,b=[1,2]
+print(a)
 
-        if a>b:
-            a,b=b,a
-        for i in range(a+1,b):
-            is_prime = True
-            for j in range(2,i):
-                if i%j==0:
-                    is_prime=False
-                    break
+a=1,
+print(type(a))
+print(type(1,))
 
-            if is_prime:
-                print(i,end=' ')
+a=(1,2,3)
+b=(2,1)
+print(a<b)
 
+a=[1,2,3,4]
+b=[7,8,9]
+a[1:3]='cat'
+print(a)
 
-
-
-
-
-# 6.1
-l=[3,2,1,0]
-for i in l:
-    print(i,end=' ')
-
-# 6.2
-guess_me=7
-number=1
-while True:
-    if number<guess_me:
-        print('too low')
-
-    elif number==guess_me:
-        print('found it')
-        break
-    else:
-        print('Oops')
-        break
-    number+=1
-
-print(number)
-
-6.3
-guess_me=5
-for number in range(10):
-    if number<guess_me:
-        print('too low')
-    elif number==guess_me:
-        print('found it')
-        break
-    else:
-        print('Oops')
-        break
+a=['dd','aa','vd','123','고고','23']
+b=sorted(a)
+print(a)
+print(b)
