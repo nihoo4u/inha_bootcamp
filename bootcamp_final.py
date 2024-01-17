792345bc37f879005a8c0de5f87918e3ef8136a7
pjy <nihoo4u@gmail.com>
2024-01-17 14:08:23 +0900

    literal comprehension

--- bootcamp_final.py
+++ bootcamp_final.py
@@ -1,11 +1,31 @@
-import copy
+b=[]
+for i in range(1,6):
+    b.append(i**2)
+print(b)
 
-subjects=["a","b","c"]
-a=subjects
-b=subjects.copy()
-c=list(subjects)
-d=subjects[:]
-e=copy.deepcopy(a)
-print(subjects,a,b,c,d)
-subjects[1]="x"
-print(subjects,a,b,c,d)
+
+
+a=[x**2 for x in range(1,6)]
+print(a)
+
+a=[x for x in range(1,10) if x%2==1]
+print(a)
+
+a=["a","b","c"]
+b=[1,2,3]
+for x,y in zip(a,b):
+    print(f'{x} is {y}th memory')
+
+rows=range(1,4)
+cols=range(1,3)
+for row in rows:
+    for col in cols:
+        print((row,col))
+
+
+cells=[(rol,col) for rol in rows for col in cols]
+print(cells)
+
+for a,b in cells: #unpacking
+    print(a,b)
+
