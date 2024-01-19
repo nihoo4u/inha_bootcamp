b29d95cd550559a7c135719eb46a2674f17f2011
pjy <nihoo4u@gmail.com>
2024-01-19 09:37:27 +0900

    day05_assignment

--- bootcamp_final.py
+++ bootcamp_final.py
@@ -1,25 +1,3 @@
-# import copy
-# subject=[1,2,3]
-# a=subject
-# b=subject[:]
-# c=subject.copy()
-# d=copy.deepcopy(subject)
-# print(subject,a,b,c,d)
-# print(id(subject),id(a),id(b),id(c),id(d))
-# subject[0]=100
-# print(subject,a,b,c,d)
-# print(id(subject),id(a),id(b),id(c),id(d))
 
-
-import copy
-subject=2
-a=subject
-
-d=copy.deepcopy(subject)
-print(subject,a,d)
-print(id(subject),id(a),id(d))
-subject=3
-print(subject,a,d)
-print(id(subject),id(a),id(d))
 
 
--- day05.py
+++ day05.py
@@ -1,119 +1,40 @@
-# #copy,deepcopy 질문
-# import copy
-# subject=[1,[2],3]
-# a=subject
-# b=subject[:]
-# c=subject.copy()
-# d=copy.deepcopy(subject)
-# print(subject,a,b,c,d)
-# print(id(subject),id(a),id(b),id(c),id(d))
-# subject[1]=[0]
-# print(subject,a,b,c,d)
-# print(id(subject),id(a),id(b),id(c),id(d))
+#9.16
+# def good():
+#     a=input().split()
+#     return a
 #
-#
-# #질문
-#
-# def squares(*n): #parameter type:typle
-#     return [i**2 for i in n] #n은 typle
-#
-# def run_function(func,*n): # function, parameter type:typle
-#     return func(*n) #return type:list
-#
-# print(run_function(squares,1,2,3))
-#
-# parameter의 개수를 특정 짓기 쉽지 않을 때...
-# *(애스터리스크)는 여러 arguement를 튜플로 모아서 parameter 전달하는 역할?
-# 다른 함수로 전달된 다음에 *을 풀면 원래대로 여러개의 aurguemt가 나오나??
-#
-#
-# def gogo(args):
-#     print(args) # *의 유무..
-# gogo((1,2,3,4)) #arguement type tuple
-#
-#
-# def gogo(*args):
-#     print(*args) # *의 유무..
-# gogo(1,2,3,4) #arguement type tuple
-#
-#
-#
-# def print_args(a,b,*args):
-#     print(a)
-#     print(b)
-#     print('positinal tuple',args)
-# # print_args(5,4,3,2,1)
-#
-# a=[1,2,3,4]
-# print(*a) #원소 출력하는 것이랑 뭐가 다르지??
-# #
-#
-#
-# def my_range(first=0,last=5,step=1):
-#     number=first
-#     while number<last:
-#         yield number
-#         number+=step
-# r=my_range()
-# print(r,type(r))
-#
-# for x in r:
-#     print(x)
-#
-# for x in r:
-#     print(x)
-#
-def document_it(func):
-    def new_function(*args,**kwargs):
-        print('Running function:',func.__name__)
-        print('positional:',args)
-        print('Keyword argument:',kwargs)
+# print(good())
 
-
-        result=func(*args,**kwargs)
-        print('*args:', *args)
-        print('arg:',args,type(args))
-        print('Result:',result)
-        return result
-    return new_function
-
-def add_ints(*nums):
-    sum = 0
-    for i in nums:
-        sum += i
-    return sum
-cooler_add_ints=document_it(add_ints)
-print(cooler_add_ints)
-cooler_add_ints(3,5,6) # a='1',b='2'
-# a=(1,2,3)
-# print(*a)
-# # arg는 튜이기 때문에 add_ints()의 arguement로 부적절
-# #->*을 통해서 unpacking을 하는 것 아닌가
+#9.2
+# def get_odds(n): # return int
+#     for i in range(1,n+1,2):
+#         yield i
+# get_odds=(x for x in range(10) if x%2==1)
+# get=1
+# for i in get_odds:
+#     if get==3:
+#         print(i)
+#         break
+#     else:
+#         get+=1
+#9.3
+#시작할 떄 출력,끝날 때 출력... 뭔 말인쥐??
+def test(f): #decorator
+    def func(*args,**kwargs):
+        print('start')
+        gogo()
+        print('end')
 
 
 
-# def receive_input(a,b,*args):
-#     print(f'a:{a}')
-#     print(f'b:{b}')
-#     print('args:',*args,sep='/')
-#
-# receive_input(*input().split())
 
-# def out_func(nout):
-#
-#     def inner_func():
-#         return nout*nout
-#     return inner_func
-#
-# x=out_func(9)
-# print(x)
-# print(x())
+    return func
+def gogo():
+    for i in range(3):
+        print(i)
+
+start_func=test(gogo)
+start_func()
 
 
-# def my_range(first=0,last=5,step=1):
-#     number=first
-#     while number<last:
-#         yield number
-#
-#         number+=step
 
